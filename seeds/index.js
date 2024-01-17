a58aa7fbc63e1d492496b25707d767410b5694cf
LandonP172 <141693593+LandonP172@users.noreply.github.com>
2024-01-17 16:30:45 -0700

    honestly dont remember if i pushed this or not so just in case

--- seeds/index.js
+++ seeds/index.js
@@ -1,26 +1,32 @@
+const sequelize = require('../config/connection');
 const seedCategories = require('./category-seeds');
 const seedProducts = require('./product-seeds');
 const seedTags = require('./tag-seeds');
 const seedProductTags = require('./product-tag-seeds');
 
-const sequelize = require('../config/connection');
-
 const seedAll = async () => {
-  await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
-  await seedCategories();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
+  try {
+    await sequelize.sync({ force: true });
 
-  await seedProducts();
-  console.log('\n----- PRODUCTS SEEDED -----\n');
+    console.log('\n----- DATABASE SYNCED -----\n');
 
-  await seedTags();
-  console.log('\n----- TAGS SEEDED -----\n');
+    await seedCategories();
+    console.log('\n----- CATEGORIES SEEDED -----\n');
 
-  await seedProductTags();
-  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+    await seedProducts();
+    console.log('\n----- PRODUCTS SEEDED -----\n');
 
-  process.exit(0);
+    await seedTags();
+    console.log('\n----- TAGS SEEDED -----\n');
+
+    await seedProductTags();
+    console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+
+    process.exit(0);
+  } catch (error) {
+    console.error('Error syncing and seeding the database:', error);
+    process.exit(1);
+  }
 };
 
 seedAll();
